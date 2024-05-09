ba805cdb74c590543366f1531457585ec2349a0e
shivam-yadav2 <shivsupertech@gmail.com>
2024-05-10 00:56:24 +0530

    setup done

---
+++ src/pages/dashboard/DashboardTabs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DashboardTabs() {
+  return (
+    <div>DashboardTabs</div>
+  )
+}
+
+export default DashboardTabs
---
+++ src/pages/getData/GetData.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function GetData() {
+  return (
+    <div>GetData</div>
+  )
+}
+
+export default GetData
