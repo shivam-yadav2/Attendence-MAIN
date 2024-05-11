f223d9ab278d041167dc32c3c704e0e0173cfe65
shivam-yadav2 <shivsupertech@gmail.com>
2024-05-11 12:50:47 +0530

    router done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Login from './pages/authentication/login/Login'
+import Home from './pages/home/Home'
+import Signup from './pages/authentication/signup/Signup'
+import Error from './pages/error/Error'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      hello
+      <BrowserRouter>
+        <Routes>  
+          <Route path='/' element={ <Home/>} />
+          <Route path='/signup' element={ <Signup/>} />
+          <Route path='/login' element={ <Login/>} />
+          <Route path='/*' element={ <Error/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 function Home() {
+
+  const navigate = useNavigate()
+  function login() {
+    // navigate('/login')
+    window.location.href = '/login'
+  }
   return (
-    <div>Home</div>
+    <div>Home
+      <NavLink to='/signup'>
+        go to signup page
+      </NavLink>
+
+      <button onClick={login}>go to login</button>
+
+    </div>
   )
 }
 
